671a8f0989234053cf0b293fd141ff1553ca6466
Mohammad-adk <mohamad.adk78@gmail.com>
2019-02-21 23:56:40 +0330

    sql modified

--- sqlite.py
+++ sqlite.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import sqlite3 
 from sqlite3 import Error
 def create_connection(db_file):
@@ -29,3 +30,36 @@ def create_keypair(conn , keys):
     cur.execute(sql, keys)
     return cur.lastrowid
 
+=======
+import sqlite3 
+from sqlite3 import Error
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        print 
+        return conn
+    except Error as e:
+        print(e)
+    return None
+# cursor = conn.cursor()
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def create_keypair(conn , keys):
+
+    sql = """ INSERT INTO keys(waves_address , bitcoin_address , bitcoin_privatekey)
+    VALUES(?,?,?)"""
+    cur = conn.cursor()
+    cur.execute(sql, keys)
+    return cur.lastrowid
+
+>>>>>>> 86c4d421f517eaff051dad372c8ac6f007d15d69
